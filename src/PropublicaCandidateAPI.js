c4fb6d6dc5c215427496f37a3f7fd6ba9abdecb1
Haykal Mubin <hmubin00@gmail.com>
2021-02-27 20:25:19 +0300

    Added the new branch without propublica API call

--- src/PropublicaCandidateAPI.js
+++ src/PropublicaCandidateAPI.js
@@ -33,64 +33,8 @@ export function GoogleAPI(props) {
 
     return (
         <div>
-            <Dropdown options={officials} placeholder='Select a Candidate ..' onChange={e => oneRepVotes(e.value)}/>
+            <Dropdown options={officials} placeholder='Select a Candidate ..' />
         </div>
     )
-}
-
-// Taking the senator name from the dropdown selection and allowing the specific API calls to happen
-function oneRepVotes(name) {
-    let crpID = '';
-    let offID = '';
-    
-    //useEffect(() => {
-        fetch("https://api.propublica.org/congress/v1/116/senate/members.json", {
-            method: "GET",
-            headers: {
-                "X-API-Key": "AYZVqN2QlJkxBhkzZ4JsFd9J3cZG1SuoWNee9QoS"
-            }
-        })
-            .then(res => res.json())
-            .then(
-                (result) => {
-                    console.log(name);
-                    console.log(result);
-                    let nameArray = name.split(" ");
-                    let offFName = nameArray[0];
-                    let offLName = nameArray[nameArray.length - 1];
-                    let indexOfOfficial = result['results'][0]['members'].findIndex(x => x.first_name === offFName & x.last_name === offLName);
-                    offID = result['results'][0]['members'][indexOfOfficial]['id'];
-                    crpID = result['results'][0]['members'][indexOfOfficial]['crp_id'];
-                    console.log(offID);
-                    console.log(crpID);
-                    return fetch("https://api.propublica.org/congress/v1/members/" + offID + "/votes.json", {
-                        method: "GET",
-                        headers: {
-                            "X-API-Key": "AYZVqN2QlJkxBhkzZ4JsFd9J3cZG1SuoWNee9QoS"
-                        }
-                    });
-                }
-            )
-            .then(res => res.json())
-            .then(
-                (result) => {
-                    console.log(result);
-                    return fetch("http://www.opensecrets.org/api/?method=candSector&cid=" + crpID + "&cycle=2020&apikey=" + "36b9f9c9066b76213e6f0c51dd12a605" + "&output=json", {
-                        method: "GET"
-                    })
-                }
-            )
-            .then(res => res.json())
-            .then(
-                (result) => {
-                    console.log(result);
-                }
-            )
-    //}, [])
-    
-
-    return (
-        <p>{crpID}</p>
-    );
 }
 
