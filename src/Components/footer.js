ce40c3cad83b2e2c5d3ff07e5de9e88054531751
jhoupps <j.c.houp7575@gmail.com>
2021-03-04 19:39:04 -0800

    slight cleanup before presentation dry run

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -1,127 +1,22 @@
 import React from "react";
-import { MDBFooter, MDBContainer, MDBCol, MDBRow } from 'mdb-react-ui-kit';
+import { MDBFooter } from 'mdb-react-ui-kit';
 
 function Footer() {
   return (
     <MDBFooter backgroundColor='light' className='text-center text-lg-left'  style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
-      <MDBContainer className='p-4'>
-        <MDBRow>
-          <MDBCol lg='3' md='6' className='mb-4 mb-md-0'>
-            <h5 className='text-uppercase'>Links</h5>
-
-            <ul className='list-unstyled mb-0'>
-              <li>
-                <a href='#!' className='text-dark'>
-                  Link 1
-                </a>
-              </li>
-              <li>
-                <a href='#!' className='text-dark'>
-                  Link 2
-                </a>
-              </li>
-              <li>
-                <a href='#!' className='text-dark'>
-                  Link 3
-                </a>
-              </li>
-              <li>
-                <a href='#!' className='text-dark'>
-                  Link 4
-                </a>
-              </li>
-            </ul>
-          </MDBCol>
-
-          <MDBCol lg='3' md='6' className='mb-4 mb-md-0'>
-            <h5 className='text-uppercase mb-0'>Links</h5>
-
-            <ul className='list-unstyled'>
-              <li>
-                <a href='#!' className='text-dark'>
-                  Link 1
-                </a>
-              </li>
-              <li>
-                <a href='#!' className='text-dark'>
-                  Link 2
-                </a>
-              </li>
-              <li>
-                <a href='#!' className='text-dark'>
-                  Link 3
-                </a>
-              </li>
-              <li>
-                <a href='#!' className='text-dark'>
-                  Link 4
-                </a>
-              </li>
-            </ul>
-          </MDBCol>
-
-          <MDBCol lg='3' md='6' className='mb-4 mb-md-0'>
-            <h5 className='text-uppercase'>Links</h5>
-
-            <ul className='list-unstyled mb-0'>
-              <li>
-                <a href='#!' className='text-dark'>
-                  Link 1
-                </a>
-              </li>
-              <li>
-                <a href='#!' className='text-dark'>
-                  Link 2
-                </a>
-              </li>
-              <li>
-                <a href='#!' className='text-dark'>
-                  Link 3
-                </a>
-              </li>
-              <li>
-                <a href='#!' className='text-dark'>
-                  Link 4
-                </a>
-              </li>
-            </ul>
-          </MDBCol>
-
-          <MDBCol lg='3' md='6' className='mb-4 mb-md-0'>
-            <h5 className='text-uppercase mb-0'>Links</h5>
-
-            <ul className='list-unstyled'>
-              <li>
-                <a href='#!' className='text-dark'>
-                  Link 1
-                </a>
-              </li>
-              <li>
-                <a href='#!' className='text-dark'>
-                  Link 2
-                </a>
-              </li>
-              <li>
-                <a href='#!' className='text-dark'>
-                  Link 3
-                </a>
-              </li>
-              <li>
-                <a href='#!' className='text-dark'>
-                  Link 4
-                </a>
-              </li>
-            </ul>
-          </MDBCol>
-        </MDBRow>
-      </MDBContainer>
-
       <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
-        &copy; {new Date().getFullYear()} Copyright:{' '}
-        <a className='text-dark' href='https://mdbootstrap.com/'>
-          MDBootstrap.com
+        &copy; {new Date().getFullYear()} Through {' '}
+        <a className='text-dark' href='https://github.com/FundsToVotes/FundsToVotes-web/blob/main/LICENSE'>
+          MIT License
         </a>
+        <p>Funds to Votes recieves its data from publicly available sources, and makes no claims regarding the accuracy thereof.</p>
+        <p> Funds To Votes is a nonpartisan organization, and doesn't work for the benefit or loss of any specific candidates</p>
+        <p> Icon: Money by Max Hancock from the Noun Project</p>
+        <hr></hr>
+      <p> Check out our <a className='text-dark' href='https://github.com/FundsToVotes/FundsToVotes-web'> github </a></p>
+       
       </div>
+      
   </MDBFooter>
   );
 }
