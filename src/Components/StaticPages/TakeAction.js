63d99d43b9ee6209af10069f57c9e34f29dd5682
Grady Thompson <gradyat@uw.edu>
2021-05-05 18:44:20 -0700

    Take Action, add logos, add privacy statement

--- src/Components/StaticPages/TakeAction.js
+++ src/Components/StaticPages/TakeAction.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
 
-class TakeAction extends React.Component {
-  render() {
-    return <h2>Take Action</h2>
-  }
+function OurData() {
+    return (
+        <div style={{ display: "block", margin: "auto", width: '75%', paddingTop: '20px', backgroundColor: 'white', padding: '30px', marginTop: '30px' }}>
+            <h2>Take Action</h2>
+            <p><em>Want to contine learning more and take action?</em></p>
+
+            <h3>Learn About H.R. 1 (The For the People Act)</h3>
+            <p>House Resolution 1 is bill that will promote voter access, election integrity and security, eliminate "dark money" in politics, and make other changes to reduce corruption and improve campaign finance systems. A companion bill, S. 1, has been introduced in the Senate. Learn more about  <a href="https://hr1-cha.house.gov" target="_blank">H.R. 1 on House.gov</a> and on <a href="https://www.congress.gov/bill/117th-congress/house-bill/1" target="_blank">Congress.gov</a>.</p>
+
+            <h3>Contact Your Representative or Senators</h3>
+            <p>Did you learn something about your members of Congress' voting records or campaign funding that you'd like to comment on? Do you want them to support H.R. 1? Use the phone number or website listed on the details page to call or send a message to your reperesentative or senator.</p>
+
+            {/* <h3>Learn About Other Organizations</h3> */}
+        </div>
+    );
 }
-
-export default TakeAction
+export default OurData;
